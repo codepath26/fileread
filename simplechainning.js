ffbeaf0f491685907ae8532f54974dbec3887c23
parth thakor <parththakor2610@gmail.com>
2023-08-10 09:04:30 +0530

    adding the file data to the top of the dom

---
+++ simplechainning.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body>');
+    // Read messages from file and display
+    const messages = fs.readFileSync('message.txt', 'utf8').split('\n').filter(message => message !== '');
+    messages.forEach(message => {
+      res.write(`<p>${message}</p>`);
+    });
+    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.appendFileSync('message.txt', message + '\n'); // Append new message
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+});
+
+server.listen(4000);
